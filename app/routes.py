cf94fab7403e63e318c1ab4e8b65fd3b5dffc73b
johniparker <a02359825@usu.edu>
2023-11-28 03:33:35 +0000

    got the model and ui working together!

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,24 @@
+import sys
+import os
 import flask
 from flask import Flask, render_template, request
 import requests
+import pandas as pd
+
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
+sys.path.append(project_root)
+
 from CorgAPI.database.rds_conn import create_conn
-from CorgAPI.model.reg_model import predict_racetime
+from CorgAPI.model.reg_model import RacetimePredictor
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route('/', methods=['GET', 'POST'])
+predictor = RacetimePredictor()
 
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    predicted_racetime = None
     #if the request is sent using post
     if request.method == 'POST':
         #if we find age in the api:
@@ -17,37 +26,18 @@ def index():
         ui_weight = int(request.form['ui_weight'])
         ui_breed = request.form['ui_breed']
         ui_gender = request.form['ui_gender']
+        user_features = pd.DataFrame({
+            'age': [ui_age], 
+            'weight':[ui_weight], 
+            'breed': [ui_breed], 
+            'gender': [ui_gender]
+        })
         print(f"received user input: {ui_age}, {ui_weight}, {ui_breed}, {ui_gender}")
-        #initiate RDS connection
-        conn = create_conn()
-    
-        if conn:
-            #call function to process user input
-            processed_data = process_data(user_input, conn)
-            estimated_racetime = predict_racetime(user_features)
-            conn.close()
-            return render_template('index.html', data=processed_data, user_input=user_input)
-        else:
-            return render_template('index.html', error="Error: Unable to connect to the database.")
+        predicted_racetime = predictor.predict_racetime(user_features)
+        
+        return render_template('index.html', predicted_racetime=predicted_racetime)
     #if get request or another error, render template without data
     return render_template('index.html', data=None, user_input=None, error=None)
-def process_data(user_input, conn):
-    cursor = conn.cursor()
-    sql = f"""
-    with cte_age as (
-    select corgid
-    from corgi
-    where age = {user_input}
-    )
-    select round(avg(racetime),2) 
-    from outcome o
-    join cte_age a on o.corgid = a.corgid
-    ;
-    """
-    cursor.execute(sql)
-    data = cursor.fetchall()
-    cursor.close()
-    return data
 
 if __name__ == '__main__':
     app.run(debug=True)
--- model/reg_model.py
+++ model/reg_model.py
@@ -1,41 +1,54 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
 from CorgAPI.database.rds_conn import create_conn
 
-def load_and_train():
-    model = LinearRegression()
-    
-    conn = create_conn()
-    query = """
-    select 
-        c.corgid,
-        c.age, 
-        c.weight, 
-        c.breed, 
-        c.gender, 
-        o.racetime
-    from
-        corgi c 
-        join outcome o on c.corgid = o.corgid
-    ;
-    """
-    df = pd.read_sql(query, conn, index_col="corgid")
-    conn.close()
+class RacetimePredictor:
+    def __init__(self):
+        self.model = LinearRegression()
+        self.preprocessor = ColumnTransformer(
+        transformers=[
+            ('breed', OneHotEncoder(drop='first'), ['breed']),
+            ('gender', OneHotEncoder(drop='first'), ['gender'])
+            ],
+            remainder='passthrough'
+        )
+        self.trained_model = self.load_and_train()
         
-    X = df[['age', 'weight', 'breed', 'gender']]
-    y = df['racetime']
+    def load_and_train(self):
+        conn = create_conn()
+        query = """
+        select 
+            c.corgid,
+            c.age, 
+            c.weight, 
+            c.breed, 
+            c.gender, 
+            o.racetime
+        from
+            corgi c 
+            join outcome o on c.corgid = o.corgid
+        ;
+        """
+        df = pd.read_sql(query, conn, index_col="corgid")
+        conn.close()
             
-    model.fit(X, y)
-            
-    test_score = self.model.score(X, y)
-    print(f"model r2 score on full dataset: {test_score}")
-    
-    return model
-
-trained_model = load_and_train()
-    
-def predict_racetime(self, user_features):
-    estimated_racetime = self.model.predict([user_features])[0]
-    return round(estimated_racetime, 2)
-   
+        X = df[['age', 'weight', 'breed', 'gender']]
+        y = df['racetime']
+        
+        X_encoded = self.preprocessor.fit_transform(X)
+        self.model.fit(X_encoded, y)
+                
+        test_score = self.model.score(X_encoded, y)
+        print(f"model r2 score on full dataset: {test_score}")
+        
+        return self.model
+        
+    def predict_racetime(self, user_features):
+        features = user_features[['age', 'weight', 'breed', 'gender']]
+        #map categorical features to numerical values
+        user_features_encoded = self.preprocessor.transform(features)
+        predicted_racetime = self.trained_model.predict(user_features_encoded)[0]
+        return round(predicted_racetime, 2)
+       
